753d007877f006419afb341d3662e39c760f7406
ajh1000 <moulawola@gmail.com>
2021-05-20 20:52:37 +0900

    merged eslint files into package.json and fixed router code in main.js

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
 import { createApp } from 'vue'
 import App from './App.vue'
-import Router from 'vue-router'
+import router  from './router'
 import Store from 'vuex'
 
-
-createApp(App).use(Router).use(Store).use(ElementPlus).mount('#app')
+createApp(App).use(router).use(Store).use(ElementPlus).mount('#app')
