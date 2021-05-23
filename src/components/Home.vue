1e13241ae5a6ab70d389e20a1c52930c35cafb5a
hyejooLyum <hjool97@gmail.com>
2021-05-23 20:47:02 +0900

    test

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -38,15 +38,7 @@
       <div id="productIntro" class="productIntro">
         <p>제품 소개</p>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
+          이 페이지는 메인입니다.
         </p>
         <el-button type="primary" round @click="$router.push({ name: 'play' })">무료 플레이</el-button>
       </div>
